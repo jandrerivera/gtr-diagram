cc155ade17e0c6cf5826cac6275de7a06704490d
Jorge Rivera <jorge@jarivera.com>
2022-07-04 16:03:16 -0400

    Added icons to menu

---
+++ src/components/ControlsMenu/ControlsMenu.tsx
@@ -0,0 +1,131 @@
+import useStore from '../../store/store';
+import { NoteSymbols } from '../../store/notes.slice';
+import ControlsMenuIcon from './ControlsMenuIcon';
+import { RiDeleteBin2Line, RiDownload2Fill } from 'react-icons/ri';
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
+
+const ControlsMenu = () => {
+  const resetNotePositions = useStore((state) => state.resetNotePositions);
+
+  // const toggleChordLabel = useStore((state) => state.toggleChordLabel);
+
+  const handleExport = () => {
+    const exportRegion = document.getElementById('exportRegion');
+
+    if (exportRegion === null) {
+      return;
+    }
+    console.log('export');
+
+    toJpeg(exportRegion, { quality: 0.95 })
+      .then((dataUrl) => {
+        const link = document.createElement('a');
+        link.download = 'chord-chart.jpg';
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className=''>
+      <SymbolSelectMenu />
+
+      <ul
+        className={`
+        mx-auto mb-2
+        flex flex-col items-stretch justify-start
+        overflow-auto rounded-lg bg-white
+        border-2 divide-y divide-slate-500 border-slate-500 
+        text-xs
+        shadow-lg
+      `}
+      >
+        <li>
+          <button
+            onClick={resetNotePositions}
+            className={` p-1 w-full text-slate-500 hover:bg-neutral-200 flex justify-start items-center gap-1`}
+          >
+            <RiDeleteBin2Line className='w-6  fill-slate-500' />
+            <div>Reset</div>
+          </button>
+        </li>
+      </ul>
+
+      <ul
+        className={`
+        mx-auto mb-2
+        flex flex-col items-stretch justify-start
+        overflow-auto rounded-lg bg-white
+        border-2 divide-y divide-slate-500 border-slate-500 
+        text-xs
+        shadow-lg
+      `}
+      >
+        <li>
+          <button
+            onClick={handleExport}
+            className={` p-1 w-full text-slate-500 hover:bg-neutral-200 flex justify-start items-center gap-1`}
+          >
+            <RiDownload2Fill className='w-6 fill-slate-500' />
+            <div>Export</div>
+          </button>
+        </li>
+        {/* <li>
+        <button
+          onClick={() => toggleChordLabel()}
+          className='w-full py-2 px-4 text-slate-500 hover:bg-neutral-200 '
+        >
+          Chord Label
+        </button>
+      </li>
+       */}
+      </ul>
+    </div>
+  );
+};
+
+const SymbolSelectMenu = () => {
+  const selectedControl = useStore((state) => state.selectedControl);
+  const setSelectedControl = useStore((state) => state.setSelectedControl);
+
+  const symbolControls = Object.entries(NoteSymbols).filter(
+    ([name]) => name !== 'default' && name !== 'blank'
+  );
+
+  const onClickSymbolSelect = (clickedDymbol: NoteSymbols) => {
+    if (selectedControl === clickedDymbol) return setSelectedControl(NoteSymbols.default);
+    setSelectedControl(clickedDymbol);
+  };
+  return (
+    <ul
+      className={`
+        mx-auto overflow-hidden
+        rounded-lg shadow-lg text-xs
+        border-2 border-slate-500 bg-slate-500 
+        items-stretch justify-start
+        grid grid-cols-2 gap-[1px]
+        mb-2
+      `}
+    >
+      {symbolControls.map(([name, symbol], key) => (
+        <li key={key} className={``}>
+          <button
+            onClick={() => onClickSymbolSelect(symbol)}
+            className={`
+              w-9 p-1
+                ${selectedControl !== symbol && 'bg-white text-slate-500  md:hover:bg-neutral-200'}
+                ${selectedControl === symbol && 'bg-slate-500 text-slate-100 md:hover:bg-slate-400'}
+              `}
+          >
+            <ControlsMenuIcon symbol={symbol} />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ControlsMenu;
---
+++ src/components/ControlsMenu/ControlsMenuIcon.tsx
@@ -0,0 +1,44 @@
+import { ReactComponent as Icon_CIRCLE } from '../../assets/svg/menu_circle.svg';
+import { ReactComponent as Icon_CIRCLE_OUTLINE } from '../../assets/svg/menu_circle_outline.svg';
+import { ReactComponent as Icon_SQUARE } from '../../assets/svg/menu_square.svg';
+import { ReactComponent as Icon_SQUARE_OUTLINE } from '../../assets/svg/menu_square_outline.svg';
+import { ReactComponent as Icon_CROSS } from '../../assets/svg/menu_cross.svg';
+import { ReactComponent as Icon_CROSS_OUTLINE } from '../../assets/svg/menu_cross_outline.svg';
+import { ReactComponent as Icon_DIAMOND } from '../../assets/svg/menu_diamond.svg';
+import { ReactComponent as Icon_DIAMOND_OUTLINE } from '../../assets/svg/menu_diamond_outline.svg';
+import { ReactComponent as Icon_TRIANGLE } from '../../assets/svg/menu_triangle.svg';
+import { ReactComponent as Icon_TRIANGLE_OUTLINE } from '../../assets/svg/menu_triangle_outline.svg';
+import { ReactComponent as Icon_BARRE } from '../../assets/svg/menu_barre.svg';
+import { ReactComponent as Icon_BARRE_OUTLINE } from '../../assets/svg/menu_barre_outline.svg';
+import { ReactComponent as Icon_BARRE_SQUARE } from '../../assets/svg/menu_sbarre.svg';
+import { ReactComponent as Icon_BARRE_SQUARE_OUTLINE } from '../../assets/svg/menu_sbarre_outline.svg';
+
+import { NoteSymbols } from '../../store/notes.slice';
+
+type DisplaySymbolsType = {
+  [value in NoteSymbols]: JSX.Element | null;
+};
+
+const ControlsMenuIcon = ({ symbol }: { symbol: NoteSymbols }) => {
+  const symbolIcons: DisplaySymbolsType = {
+    BLANK: <></>,
+    DEFAULT: <></>,
+    CIRCLE: <Icon_CIRCLE className='fill-current' />,
+    SQUARE: <Icon_SQUARE className='fill-current' />,
+    DIAMOND: <Icon_DIAMOND className='fill-current' />,
+    TRIANGLE: <Icon_TRIANGLE className='fill-current' />,
+    CROSS: <Icon_CROSS className='fill-current' />,
+    CIRCLE_OUTLINE: <Icon_CIRCLE_OUTLINE className='fill-current' />,
+    SQUARE_OUTLINE: <Icon_SQUARE_OUTLINE className='fill-current' />,
+    DIAMOND_OUTLINE: <Icon_DIAMOND_OUTLINE className='fill-current' />,
+    TRIANGLE_OUTLINE: <Icon_TRIANGLE_OUTLINE className='fill-current' />,
+    CROSS_OUTLINE: <Icon_CROSS_OUTLINE className='fill-current' />,
+    BARRE: <Icon_BARRE className='fill-current' />,
+    BARRE_OUTLINE: <Icon_BARRE_OUTLINE className='fill-current' />,
+    BARRE_SQUARE: <Icon_BARRE_SQUARE className='fill-current' />,
+    BARRE_SQUARE_OUTLINE: <Icon_BARRE_SQUARE_OUTLINE className='fill-current' />,
+  };
+  return symbolIcons[symbol];
+};
+
+export default ControlsMenuIcon;
